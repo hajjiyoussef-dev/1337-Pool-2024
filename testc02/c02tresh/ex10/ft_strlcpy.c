9763927716cee2edef16035a9baf2d28a5131912
youssef_hajji <118576019+hajjiyoussef-dev@users.noreply.github.com>
2024-08-03 21:14:33 +0100

    kh-1337

---
+++ testc02/c02tresh/ex10/ft_strlcpy.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yhajji <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 12:08:36 by yhajji            #+#    #+#             */
+/*   Updated: 2024/08/03 12:59:34 by yhajji           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char	*c)
+{
+	int	i;
+
+	i = 0;
+	while (c[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	len;
+	unsigned int	i;
+
+	i = 0;
+	len = ft_strlen(src);
+	if (size != 0)
+	{
+		while (src[i])
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (len);
+}
