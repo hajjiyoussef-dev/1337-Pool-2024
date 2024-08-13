24fd2669c8e724f0dc96ee99aa3568334809b151
youssef_hajji <118576019+hajjiyoussef-dev@users.noreply.github.com>
2024-08-13 22:22:59 +0100

    kh-2024-pool-2024

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yhajji <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/13 21:43:22 by yhajji            #+#    #+#             */
+/*   Updated: 2024/08/13 22:07:11 by yhajji           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char	*str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_swap(char **s1, char **s2)
+{
+	char	*help;
+
+	help = *s1;
+	*s1 = *s2;
+	*s2 = help;
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (argc > i)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+			{
+				ft_swap(&argv[i], &argv[j]);
+			}
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	while (argc - 1 >= i)
+	{
+		ft_putstr(argv[i++]);
+	}
+	return (0);
+}
